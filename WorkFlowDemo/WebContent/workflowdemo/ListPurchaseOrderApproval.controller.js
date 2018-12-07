0f367b88c78a467b2a237be448013b59e89abb26
diegofrutuoso <diegofrutuosomachado@gmail.com>
2018-12-07 16:15:45 -0200

    Add List

--- WorkFlowDemo/WebContent/workflowdemo/ListPurchaseOrderApproval.controller.js
+++ WorkFlowDemo/WebContent/workflowdemo/ListPurchaseOrderApproval.controller.js
@@ -1,18 +1,46 @@
-sap.ui.controller("workflowdemo.MainPage", {
+sap.ui.controller("workflowdemo.ListPurchaseOrderApproval", {
 
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf workflowdemo.MainPage
+* @memberOf workflowdemo.ListPurchaseOrderApproval
 */
-//	onInit: function() {
-//
-//	},
+	onInit: function() {
+
+		var oData = {
+				itens:[
+					{
+						id: "1",
+						fornecedor: "Diego F. Machado",
+						valor: "20000",
+						vencto: "21/10/2019"
+					},
+					{
+						id: "2",
+						fornecedor: "Ultracon Consultoria",
+						valor: "59652",
+						vencto: "22/12/2018"
+					},
+					{
+						id: "3",
+						fornecedor: "Ri Happy",
+						valor: "199",
+						vencto: "01/01/2019"
+					}
+				]
+			}
+		
+		var oModel = new sap.ui.model.json.JSONModel();
+		oModel.setData(oData);
+		
+		sap.ui.getCore().setModel(oModel);
+		
+	},
 
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf workflowdemo.MainPage
+* @memberOf workflowdemo.ListPurchaseOrderApproval
 */
 //	onBeforeRendering: function() {
 //
@@ -21,7 +49,7 @@ sap.ui.controller("workflowdemo.MainPage", {
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf workflowdemo.MainPage
+* @memberOf workflowdemo.ListPurchaseOrderApproval
 */
 //	onAfterRendering: function() {
 //
@@ -29,16 +57,10 @@ sap.ui.controller("workflowdemo.MainPage", {
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf workflowdemo.MainPage
+* @memberOf workflowdemo.ListPurchaseOrderApproval
 */
 //	onExit: function() {
 //
 //	}
 	
-	press: function(oEvt){
-		
-		app.to("idListPage");
-		
-	}
-
 });
---
+++ WorkFlowDemo/WebContent/workflowdemo/ListPurchaseOrderApproval.view.js
@@ -0,0 +1,43 @@
+sap.ui.jsview("workflowdemo.ListPurchaseOrderApproval", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf workflowdemo.ListPurchaseOrderApproval
+	*/ 
+	getControllerName : function() {
+		return "workflowdemo.ListPurchaseOrderApproval";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf workflowdemo.ListPurchaseOrderApproval
+	*/ 
+	createContent : function(oController) {
+		
+		var oList = new sap.m.List({
+			headerText:"Pendentes"
+		});
+		
+		oList.bindAggregation(
+				"items",
+				"/itens",
+				 new sap.m.StandardListItem({
+					title: "{fornecedor}",
+					description: " Venct " + "{vencto}" + "  -  R$" + "{valor}",
+					type: sap.m.ListType.Navigation
+				})
+		);
+		
+ 		var oPage = new sap.m.Page({
+			title: "Pedidos de Compra",
+			showNavButton:true,
+			navButtonPress: function(oEvt){app.back();},
+			content: [
+				oList
+			]
+		});
+ 		
+ 		return oPage;
+	}
+
+});
